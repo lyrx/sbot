8d8aefa667eca00e3652af48ef3ed0b4b0bb6860
alex <enna@mailbox.org>
2023-09-20 16:18:10 +0200

    Betterch check functions

--- src/solidity_checks.py
+++ src/solidity_checks.py
@@ -8,8 +8,7 @@ def check_outdated_versions(code):
     results = []
     outdated_versions = ['0.4.', '0.5.']
     for version in outdated_versions:
-        match = re.search(version, code)
-        if match:
+        for match in re.finditer(version, code):
             line_no = get_line_number(code, match.start())
             message = f"Warning: The code seems to be using an outdated Solidity version ({version}x)."
             results.append({'line': line_no, 'message': message})
@@ -17,8 +16,7 @@ def check_outdated_versions(code):
 
 def check_pragma_experimental(code):
     results = []
-    match = re.search('pragma experimental', code)
-    if match:
+    for match in re.finditer('pragma experimental', code):
         line_no = get_line_number(code, match.start())
         message = "Warning: The code uses 'pragma experimental'. This can introduce instability."
         results.append({'line': line_no, 'message': message})
@@ -27,8 +25,7 @@ def check_pragma_experimental(code):
 def check_send_transfer(code):
     results = []
     send_transfer_pattern = re.compile(r'\.send\(|\.transfer\(')
-    match = send_transfer_pattern.search(code)
-    if match:
+    for match in send_transfer_pattern.finditer(code):
         line_no = get_line_number(code, match.start())
         message = "Warning: 'send' or 'transfer' detected. Ensure you handle their potential failures."
         results.append({'line': line_no, 'message': message})
@@ -36,8 +33,7 @@ def check_send_transfer(code):
 
 def check_low_level_call(code):
     results = []
-    match = re.search(r'\.call\(', code)
-    if match:
+    for match in re.finditer(r'\.call\(', code):
         line_no = get_line_number(code, match.start())
         message = "Warning: Low-level calls can be dangerous. Ensure you handle their potential failures and avoid reentrancy attacks."
         results.append({'line': line_no, 'message': message})
@@ -45,8 +41,7 @@ def check_low_level_call(code):
 
 def check_tx_origin(code):
     results = []
-    match = re.search('tx.origin', code)
-    if match:
+    for match in re.finditer('tx.origin', code):
         line_no = get_line_number(code, match.start())
         message = "Warning: 'tx.origin' detected. It can be manipulated by malicious contracts. Consider using 'msg.sender' instead."
         results.append({'line': line_no, 'message': message})
@@ -56,8 +51,7 @@ def check_visibility_specifiers(code):
     results = []
     visibility_specifiers = ['public', 'private', 'internal', 'external']
     function_pattern = re.compile(r'function\s+(\w+)\s*\(')
-    matches = function_pattern.finditer(code)
-    for match in matches:
+    for match in function_pattern.finditer(code):
         function = match.group(1)
         if not any(vis in function for vis in visibility_specifiers):
             line_no = get_line_number(code, match.start())
