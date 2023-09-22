cada7654e31699ee486f2d8c8b3f8ab3206e54c4
alex <enna@mailbox.org>
2023-09-22 08:40:38 +0200

    Refactoring

--- src/analyzefile.py
+++ src/analyzefile.py
@@ -1,22 +1,5 @@
-from solidity_checks import (
-    check_outdated_versions,
-    check_pragma_experimental,
-    check_send_transfer,
-    check_low_level_call,
-    check_tx_origin,
-    check_visibility_specifiers
-)
 from solidity_analyzer import analyze_solidity_file  # Import the externalized function
-
-
-check_structures = [
-    {"name": "Outdated Versions", "func": check_outdated_versions},
-    {"name": "Pragma Experimental", "func": check_pragma_experimental},
-    {"name": "Send/Transfer", "func": check_send_transfer},
-    {"name": "Low-Level Call", "func": check_low_level_call},
-    {"name": "Tx.Origin", "func": check_tx_origin},
-    {"name": "Visibility Specifiers", "func": check_visibility_specifiers}
-]
+from bloodhounds import check_structures
 
 # Test
 file_path = "/Users/alex/git/Web3Academy/hats-audit/contracts/CvgControlTower.sol"
---
+++ src/bloodhounds.py
@@ -0,0 +1,17 @@
+from solidity_checks import (
+    check_outdated_versions,
+    check_pragma_experimental,
+    check_send_transfer,
+    check_low_level_call,
+    check_tx_origin,
+    check_visibility_specifiers
+)
+
+check_structures = [
+    {"name": "Outdated Versions", "func": check_outdated_versions},
+    {"name": "Pragma Experimental", "func": check_pragma_experimental},
+    {"name": "Send/Transfer", "func": check_send_transfer},
+    {"name": "Low-Level Call", "func": check_low_level_call},
+    {"name": "Tx.Origin", "func": check_tx_origin},
+    {"name": "Visibility Specifiers", "func": check_visibility_specifiers}
+]
